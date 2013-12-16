9f68a96180187a4af535f22988279b2021d1d5b2
Matt Long <matt.long@matthew-long.com>
2013-12-16 13:55:35 -0700

    Initial Commit

---
+++ GradientPieLayer/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GradientPieLayer
+//
+//  Created by Matt Long on 12/16/13.
+//  Copyright (c) 2013 Matt Long. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MLAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MLAppDelegate class]));
+    }
+}
